d514f9176b54a534eec4a91a8cc858dcd2c03d9d
Rohith Kumar Goggura <rohithkumar.goggura@gmail.com>
2019-11-29 23:43:48 +0530

    Created login,logout authenticate

---
+++ AuthenticateApp-UI/src/environments/environment.ts
@@ -0,0 +1,10 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiBaseUrl: "http://localhost:3000/api"
+};
+
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
---
+++ README.md
@@ -0,0 +1,7 @@
+# MEAN Stack Login and Logout in Angular
+
+MEAN Stack Project inside that I have designed Login Form and Related Logout feature with angular 4 and above
+
+###### Before Running this Project
+
+Install npm packages using 'npm install' command from AuthenticateApp-UI and server folder.
